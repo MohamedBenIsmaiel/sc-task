bca78990b762e86f6526a9bb97091b4b5836e9da
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-11-01 19:12:46 +0200

    init config

--- config/config.js
+++ config/config.js
@@ -1,24 +1,33 @@
 module.exports = {
-  development: {
-    username: 'mismaiel',
-    password: '1234',
-    database: 'secondProfit',
-    host: '127.0.0.1',
-    dialect: 'postgres',
-    port: '5434'
+  db: {
+    development: {
+      username: 'mismaiel',
+      password: '1234',
+      database: 'secondProfit',
+      host: '127.0.0.1',
+      dialect: 'postgres',
+      port: '5434'
+    },
+    test: {
+      username: 'root',
+      password: null,
+      database: 'database_test',
+      host: '127.0.0.1',
+      dialect: 'mysql'
+    },
+    production: {
+      username: 'root',
+      password: null,
+      database: 'database_production',
+      host: '127.0.0.1',
+      dialect: 'mysql'
+    }
   },
-  test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'mysql'
+  githubOption: {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36'
+    },
+    OAUth: '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'
   },
-  production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql'
-  }
+  githubApiUrl: 'https://api.github.com/users'
 }
