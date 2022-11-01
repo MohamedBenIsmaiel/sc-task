7d089633624a584da944f26d9e0b5795859d8a47
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-11-01 19:11:56 +0200

    update user model & migration

--- models/user.js
+++ models/user.js
@@ -32,6 +32,46 @@ module.exports = (sequelize, DataTypes) => {
     location: {
       type: DataTypes.STRING,
       allowNull: true
+    },
+    avatar: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    company: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    blog: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    bio: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    twitterUserName: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    publicRepos: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    followers: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    following: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    githubJoinedAt: {
+      type: DataTypes.STRING,
+      allowNull: true
     }
   }, {
     sequelize,
