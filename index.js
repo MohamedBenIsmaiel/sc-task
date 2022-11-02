09cd7518f7ef8c84811baa089ecc404a6a9aab6c
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-11-02 18:33:36 +0200

    adding CMD

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-const { Sequelize } = require('sequelize')
-const db = require('./models/index')
+#!/usr/bin/env node
+const { getArgsOption, runCMD } = require('./CMD/cmd')
 
-const sequelize = new Sequelize('postgres://mismaiel:1234@127.0.0.1:5434/secondProfit');
-
-(async function test () {
-  try {
-    await sequelize.authenticate()
-    console.log('Connection has been established successfully.')
-    const [user] = await db.User.findOrCreate({
-      where: { userName: 'Mohamed' }
-    })
-    console.log('###### users ', user)
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
-  }
-})()
+runCMD(getArgsOption())
