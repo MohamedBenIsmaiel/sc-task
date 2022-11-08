6191011adf3c4c6ed1f261c4fbe11a86d34f25e6
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2022-11-08 20:20:35 +0200

    install dependency first

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 echo "[*] Running docker-compose , database container will be up ..";
 docker-compose up -d;
-echo "[*] Running migration ";
-npm run migrate;
+
 
 echo "[*] Installing dependencies ";
 npm i;
+
+echo "[*] Running migration ";
+npm run migrate;
+
 npm i -g;
 echo "[*] we are done ;) ";
